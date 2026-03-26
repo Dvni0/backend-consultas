ff687b2daf7863edc1f38771e3c787ed33fe2c5c
Dvni0 <viniciusb32@gmail.com>
2026-03-26 00:41:15 -0300

    feat: criação do serviço do Paciente

--- src/main/java/com/fiap/ec/backend_consultas/repository/PacienteRepository.java
+++ src/main/java/com/fiap/ec/backend_consultas/repository/PacienteRepository.java
@@ -3,4 +3,5 @@ import com.fiap.ec.backend_consultas.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository  extends  JpaRepository<Paciente,Long> {
+    Long id(Long id);
 }
